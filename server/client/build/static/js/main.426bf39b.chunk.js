fa9c80d5178493c75aec80d5817b8c5f7c4cde30
Michele Minno <michele.minno@gmail.com>
2020-09-27 18:31:21 +0200

    package.json updated and build folder removed

--- server/client/build/static/js/main.426bf39b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmeteo-reports"]=this["webpackJsonpmeteo-reports"]||[]).push([[0],{12:function(e,t,o){"use strict";o.r(t);var n=o(0),r=o.n(n),s=o(2),p=o.n(s),a=o(3),c=o(4),m=o(6),u=o(5);r.a.Component;p.a.render(r.a.createElement("h1",null," Meteo reports "),document.getElementById("root"))},7:function(e,t,o){e.exports=o(12)}},[[7,1,2]]]);
-//# sourceMappingURL=main.426bf39b.chunk.js.map
