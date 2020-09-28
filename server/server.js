0bfd6d37fe0c055b7041299e53e88f1abbe600ed
Michele Minno <michele.minno@gmail.com>
2020-09-28 12:03:30 +0200

    PORT prop in env file added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const port = process.env.DEV_APP_PORT || process.env.PORT
+const port = process.env.DEV_APP_PORT || process.env.PORT;
 
 app.listen(port, () => {
 
