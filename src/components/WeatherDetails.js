8d792b0302389eedcd044263c60408c3d1dca558
Michele Minno <michele.minno@gmail.com>
2020-10-05 17:46:14 +0200

    wind speed updated (a fix)

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -34,7 +34,7 @@ const WeatherDetails = ({location}) => {
                                  <div className="date">{detail.dt_txt}</div>
                                  <div className="temperature">{detail.main.temp}</div>
                                  <div className="description">{detail.weather[0].description}</div>
-                                 <div className="wind">wind: {detail.wind.speed} km/h</div>
+                                 <div className="wind">wind: {detail.wind.speed}</div>
                              </div>
                      );
                  })
